ee8bff6d1a721f8942510ebc6880881933904f19
JesusGlezAcosta <71987633+JesusGlezAcosta@users.noreply.github.com>
2022-06-01 00:27:19 -0800

    Contador de click agregado

--- app/src/main/java/gonzalez/jesus/myfirstapp/MainActivity.kt
+++ app/src/main/java/gonzalez/jesus/myfirstapp/MainActivity.kt
@@ -2,10 +2,24 @@ package gonzalez.jesus.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.btnClick)
+        val tvTextView = findViewById<TextView>(R.id.textView)
+        var timesClicked = 0
+
+        btnClickMe.setOnClickListener{
+            timesClicked += 1
+
+            tvTextView.text = timesClicked.toString()
+            Toast.makeText(this, "Sigue aplastado el botón", Toast.LENGTH_SHORT).show()
+        }
     }
 }
